ea7be3e981b93a94dabb96cd4f2c1609c5193f3c
dcjones1 <dcejjones@gmail.com>
2019-03-13 10:16:13 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,68 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleFieldChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmission = (event) => {
+    event.preventDefault()
+    let obj = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    if (obj.username === '' || obj.password === '') {
+      window.alert('Please enter information into both fields.')
+    } else {
+      this.props.onSubmit(obj)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmission}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleFieldChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleFieldChange}
+            />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button
+            type="submit"
+          >
+            Log in
+          </button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
   }
+
+  onChange = (event) => {
+    this.setState({tweet: event.target.value})
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.onChange} />
+        <strong>Chars left: {this.props.maxChars - this.state.tweet.length}</strong>
       </div>
     );
   }
